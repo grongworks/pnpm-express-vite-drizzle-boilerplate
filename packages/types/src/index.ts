a78747a650a109a65e840fc752b18510fe4c04f2
grongworks <christian.koethe@grongworks.de>
2026-02-11 14:17:54 +0100

    add new types for ChangeMeIfNeeded and CreateChangeMeIfNeededRequest; update package.json to include @tanstack/react-query and related dependencies

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,3 +3,13 @@
 export type HelloMessage = {
     status: string;
 }
+
+export type ChangeMeIfNeeded = {
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export type CreateChangeMeIfNeededRequest = {
+    // Extend as needed
+}
